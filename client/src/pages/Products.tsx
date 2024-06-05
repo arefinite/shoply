dda407431bb91e29287b083a727712a5cf77483d
arefinite <arefinite@gmail.com>
2024-06-05 18:24:23 +0600

    dashboard home

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Button } from '@/components/ui/button';
+import { useState, useEffect } from 'react'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -7,141 +7,144 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { useGetAllProducts } from '@/services/queries';
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { useGetAllProducts } from '@/services/queries'
 
-import { LoaderCircle, Star } from 'lucide-react';
-import { Product } from '@/types/product';
+import { LoaderCircle, Star } from 'lucide-react'
+import { Product } from '@/types/product'
 
 const Products = () => {
   const {
     data: products,
     isPending: isProductsPending,
     isError: isProductsError,
-  } = useGetAllProducts();
+  } = useGetAllProducts()
 
-  const [searchName, setSearchName] = useState('');
-  const [searchCategory, setSearchCategory] = useState('');
-  const [searchRating, setSearchRating] = useState('');
-  const [searchBrand, setSearchBrand] = useState('');
-  const [filteredProducts, setFilteredProducts] = useState<Product[]| never>([]);
+  const [searchName, setSearchName] = useState('')
+  const [searchCategory, setSearchCategory] = useState('')
+  const [searchRating, setSearchRating] = useState('')
+  const [searchBrand, setSearchBrand] = useState('')
+  const [filteredProducts, setFilteredProducts] = useState<Product[] | never>(
+    []
+  )
 
   useEffect(() => {
     if (products) {
-      let filtered = products;
+      let filtered = products
 
       if (searchName) {
         filtered = filtered.filter(product =>
           product.title.toLowerCase().includes(searchName.toLowerCase())
-        );
+        )
       }
 
       if (searchCategory) {
         filtered = filtered.filter(product =>
           product.category.toLowerCase().includes(searchCategory.toLowerCase())
-        );
+        )
       }
 
       if (searchRating) {
-        filtered = filtered.filter(product =>
-          +product.rating === parseInt(searchRating)
-        );
+        filtered = filtered.filter(
+          product => +product.rating === parseInt(searchRating)
+        )
       }
 
       if (searchBrand) {
         filtered = filtered.filter(product =>
           product.brand.toLowerCase().includes(searchBrand.toLowerCase())
-        );
+        )
       }
 
-      setFilteredProducts(filtered);
+      setFilteredProducts(filtered)
     }
-  }, [products, searchName, searchCategory, searchRating, searchBrand]);
+  }, [products, searchName, searchCategory, searchRating, searchBrand])
 
   if (isProductsPending)
     return (
-      <div className="flex justify-center items-center h-screen">
-        <LoaderCircle className="animate-spin" />
+      <div className='flex justify-center items-center h-screen'>
+        <LoaderCircle className='animate-spin' />
       </div>
-    );
-  if (isProductsError) return <p>Something went wrong!</p>;
+    )
+  if (isProductsError) return <p>Something went wrong!</p>
 
   return (
-    <main className="center pb-8">
-      <h2 className="text-2xl text-center font-semibold py-8">Discounted Gadgets</h2>
-      <div className="flex flex-col md:flex-row gap-4">
-        <Card className="md:w-[250px] border h-fit p-4 sticky top-0 bg-muted">
-          <h1 className="text-center font-semibold pb-4">Search by</h1>
+    <main className='center pb-8'>
+      <h2 className='text-2xl text-center font-semibold py-8'>All Products</h2>
+      <div className='flex flex-col md:flex-row gap-4'>
+        <Card className='md:w-[250px] border h-fit p-4 sticky top-0 bg-muted'>
+          <h1 className='text-center font-semibold pb-4'>Search by</h1>
           <div>
-            <form className="space-y-4">
+            <form className='space-y-4'>
               <Input
-                placeholder="Search by name"
+                placeholder='Search by name'
                 value={searchName}
-                onChange={(e) => setSearchName(e.target.value)}
+                onChange={e => setSearchName(e.target.value)}
               />
               <Input
-                placeholder="Search by category"
+                placeholder='Search by category'
                 value={searchCategory}
-                onChange={(e) => setSearchCategory(e.target.value)}
+                onChange={e => setSearchCategory(e.target.value)}
               />
               <Input
-                placeholder="Search by rating"
+                placeholder='Search by rating'
                 value={searchRating}
-                onChange={(e) => setSearchRating(e.target.value)}
+                onChange={e => setSearchRating(e.target.value)}
               />
               <Input
-                placeholder="Search by brand"
+                placeholder='Search by brand'
                 value={searchBrand}
-                onChange={(e) => setSearchBrand(e.target.value)}
+                onChange={e => setSearchBrand(e.target.value)}
               />
-              <Button className="w-full" onClick={(e) => e.preventDefault()}>
+              <Button className='w-full' onClick={e => e.preventDefault()}>
                 Search
               </Button>
             </form>
           </div>
         </Card>
-        <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {filteredProducts.map((product) => (
+        <section className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {filteredProducts.map(product => (
             <Card key={product._id}>
               <CardHeader>
                 <img
                   src={product.image}
                   alt={product.title}
-                  className="w-full h-36 object-cover"
+                  className='w-full h-36 object-cover'
                 />
-                <CardTitle className="pt-4">{product.title}</CardTitle>
+                <CardTitle className='pt-4'>{product.title}</CardTitle>
                 <CardDescription>
                   <span>Category: {product.category}</span>
                 </CardDescription>
               </CardHeader>
-              <CardContent className="flex gap-2 flex-col">
-                <div className="flex flex-col gap-1">
+              <CardContent className='flex gap-2 flex-col'>
+                <div className='flex flex-col gap-1'>
                   <span>
                     Price: <strong>${product.price}</strong>
                   </span>
-                  <span className="flex gap-1 items-center">
+                  <span className='flex gap-1 items-center'>
                     Rating: <strong>{product.rating}</strong> <Star size={20} />
                   </span>
                 </div>
-                <div className="flex flex-col gap-1">
+                <div className='flex flex-col gap-1'>
                   <span>
-                    Before: <span className="line-through">${product.price}</span>
+                    Before:{' '}
+                    <span className='line-through'>${product.price}</span>
                   </span>
-                  <span className="font-semibold">
+                  <span className='font-semibold'>
                     Price: $
                     {product.discount &&
                       (
                         +product.price -
                         Math.ceil((+product.price * +product.discount) / 100)
                       ).toFixed(2)}{' '}
-                    <span className="text-sm text-red-500">
+                    <span className='text-sm text-red-500'>
                       ({product.discount}% discount)
                     </span>
                   </span>
                 </div>
               </CardContent>
-              <CardFooter className="flex justify-between">
+              <CardFooter className='flex justify-between'>
                 <Button>View Details</Button>
               </CardFooter>
             </Card>
@@ -149,7 +152,7 @@ const Products = () => {
         </section>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
--- client/src/pages/dashboard/DashboardHome.tsx
+++ client/src/pages/dashboard/DashboardHome.tsx
@@ -1,6 +1,56 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { useGetAllProducts } from "@/services/queries";
+import { Product } from '@/types/product';
+import { LoaderCircle } from 'lucide-react';
+
 const DashboardHome = () => {
+  const {
+    data: products,
+    isPending: isProductsPending,
+    isError: isProductsError,
+  } = useGetAllProducts();
+
+  if (isProductsPending) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <LoaderCircle className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (isProductsError) {
+    return <p>Something went wrong!</p>;
+  }
+
+  const productsList = products as Product[];
+
+  const categoryCounts = productsList.reduce((acc: Record<string, number>, product: Product) => {
+    if (!acc[product.category]) {
+      acc[product.category] = 0;
+    }
+    acc[product.category]++;
+    return acc;
+  }, {});
+
   return (
-    <main className="mt-8">DashboardHome</main>
-  )
-}
-export default DashboardHome
+    <main className="mt-8">
+      <h1 className="text-2xl text-center font-semibold mb-8">Dashboard Home</h1>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {Object.keys(categoryCounts).map((category) => (
+          <Card key={category} className="p-4">
+            <CardHeader>
+              <CardTitle>{category}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>
+                Number of Products: <strong>{categoryCounts[category]}</strong>
+              </CardDescription>
+            </CardContent>
+          </Card>
+        ))}
+      </section>
+    </main>
+  );
+};
+
+export default DashboardHome;
