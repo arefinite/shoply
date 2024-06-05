c06a54107cbbaad322829b54dd49af87b9ba4635
arefinite <arefinite@gmail.com>
2024-06-05 19:07:51 +0600

    recharts implemented

--- client/src/pages/dashboard/DashboardHome.tsx
+++ client/src/pages/dashboard/DashboardHome.tsx
@@ -1,7 +1,10 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useGetAllProducts } from "@/services/queries";
 import { Product } from '@/types/product';
 import { LoaderCircle } from 'lucide-react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
 
 const DashboardHome = () => {
   const {
@@ -22,8 +25,10 @@ const DashboardHome = () => {
     return <p>Something went wrong!</p>;
   }
 
+  
   const productsList = products as Product[];
 
+  
   const categoryCounts = productsList.reduce((acc: Record<string, number>, product: Product) => {
     if (!acc[product.category]) {
       acc[product.category] = 0;
@@ -32,22 +37,34 @@ const DashboardHome = () => {
     return acc;
   }, {});
 
+  const data = Object.keys(categoryCounts).map(category => ({
+    category,
+    count: categoryCounts[category]
+  }));
+
   return (
     <main className="mt-8">
       <h1 className="text-2xl text-center font-semibold mb-8">Dashboard Home</h1>
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {Object.keys(categoryCounts).map((category) => (
-          <Card key={category} className="p-4">
-            <CardHeader>
-              <CardTitle>{category}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <CardDescription>
-                Number of Products: <strong>{categoryCounts[category]}</strong>
-              </CardDescription>
-            </CardContent>
-          </Card>
-        ))}
+      <section className="grid grid-cols-1 gap-4">
+        <Card className="p-4">
+          <CardHeader>
+            <CardTitle>Products by Category</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart
+                data={data}
+                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+              >
+                <XAxis dataKey="category" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="count" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </Card>
       </section>
     </main>
   );
